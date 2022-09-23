1920b7b73072505e5d4ee4a36c67a2b6e340a1ff
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-09-22 23:53:39 -0700

    Added sane defaults

--- neovim/init.lua
+++ neovim/init.lua
@@ -2,6 +2,7 @@ require("plugins")
 require("nvim-tree").setup()
 require('lualine').setup {options = { theme = 'gruvbox' }}
 require("lsp")
+require("bruce.sanity")
 require("bruce.commands")
 require("bruce.keymaps")
 require("bruce.colors")
