a562d81d91d4b5813db6d49ff2fa29c9895d73ec
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-09-30 11:24:56 -0700

    use light theme between 9 am and 6 pm

--- neovim/lua/bruce/sanity.lua
+++ neovim/lua/bruce/sanity.lua
@@ -15,3 +15,19 @@ vim.cmd("set incsearch")
 vim.cmd("set signcolumn=number")
 vim.cmd("set nohlsearch")
 vim.cmd("set cursorline")
+
+-- Hours between 9 am and 6 pm, use light theme; else use dark theme
+load_dt = os.date("*t", os.time())
+light_hour_start = 9
+light_hour_stop = 18
+
+if load_dt.hour >= light_hour_start and load_dt.hour < light_hour_stop then
+    print("Use light theme")
+    vim.cmd("colorscheme PaperColor")
+    vim.cmd("set bg=light")
+else
+    print("Use dark theme")
+    vim.cmd("colorscheme gruvbox")
+    vim.cmd("set bg=dark")
+end
+
