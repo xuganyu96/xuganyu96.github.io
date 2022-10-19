cdb90f12b625721b236b29f8d1085e2f288582f0
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-10-18 22:29:53 -0700

    Added rust-analyzer to startup

--- neovim/lua/lsp.lua
+++ neovim/lua/lsp.lua
@@ -41,3 +41,11 @@ require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
