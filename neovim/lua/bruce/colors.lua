1b9ec715acb18341e8c41b1859ae8063d7a6acad
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2022-12-09 17:05:52 -0800

    Vancouver's sun sets at 4 pm now

--- neovim/lua/bruce/colors.lua
+++ neovim/lua/bruce/colors.lua
@@ -1,7 +1,7 @@
 -- Hours between 9 am and 6 pm, use light theme; else use dark theme
 load_dt = os.date("*t", os.time())
 light_hour_start = 9
-light_hour_stop = 18
+light_hour_stop = 16
 
 if load_dt.hour >= light_hour_start and load_dt.hour < light_hour_stop then
     vim.cmd("colorscheme PaperColor")
