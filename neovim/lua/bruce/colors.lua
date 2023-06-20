5b939e4332a2b71a550879ba6d43ba0e35f0efc9
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-06-19 23:07:36 -0700

    Set transparent mode

--- neovim/lua/bruce/colors.lua
+++ neovim/lua/bruce/colors.lua
@@ -1,7 +1,24 @@
+require("gruvbox").setup({
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = true,
+})
 vim.cmd("colorscheme gruvbox")
 vim.cmd("set bg=dark")
-
-vim.cmd("highlight Normal ctermbg=none")
-vim.cmd("highlight NonText ctermbg=none")
-vim.cmd("highlight Normal guibg=none")
-vim.cmd("highlight NonText guibg=none")
