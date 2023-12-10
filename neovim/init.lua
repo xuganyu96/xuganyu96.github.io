8552df8311774a85dc5bee57d163bfb0e0b6acb9
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-12-10 14:50:57 -0500

    Revert "Update ident-blankline"
    This reverts commit 4077bb4d27acaba1919b2e5348a1d42fa877b6e9.

--- neovim/init.lua
+++ neovim/init.lua
@@ -205,12 +205,13 @@ require('lazy').setup({
 
   {
     -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
     opts = {
       char = '┊',
-      show_trailing_blankline_indent = false
-    }
+      show_trailing_blankline_indent = false,
+    },
   },
 
   -- "gc" to comment visual regions/lines
