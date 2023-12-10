4077bb4d27acaba1919b2e5348a1d42fa877b6e9
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-12-10 14:47:53 -0500

    Update ident-blankline

--- neovim/init.lua
+++ neovim/init.lua
@@ -205,13 +205,12 @@ require('lazy').setup({
 
   {
     -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     opts = {
       char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+      show_trailing_blankline_indent = false
+    }
   },
 
   -- "gc" to comment visual regions/lines
