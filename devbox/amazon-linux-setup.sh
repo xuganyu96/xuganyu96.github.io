2fa287336813686dae6b6c00f683b12467a2e914
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-07-25 00:46:45 -0400

    Streamlined setup

--- devbox/amazon-linux-setup.sh
+++ devbox/amazon-linux-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 setup_pyenv() {
+    echo ">>>>>>>> Installing pyenv"
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     cd ~/.pyenv && src/configure && make -C src
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
@@ -10,56 +11,68 @@ setup_pyenv() {
     echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
     echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
     source ~/.bashrc
-    pyenv install 3.12.4 --verbose
+    echo "Installed pyenv <<<<<<<<<<"
+    echo ">>>>>>>> Installing Python 3.12.4"
+    pyenv install 3.12.4
     pyenv global 3.12.4
     python -m pip install --upgrade pip setuptools wheel
+    echo "Installed Python 3.12.4 <<<<<<<<<<"
 }
 
 setup_rust() {
+    echo ">>>>>>>> Installing Rust"
     curl https://sh.rustup.rs -sSf | sh -s -- -y
     source ~/.cargo/env
+    echo "Installed Rust <<<<<<<<<<"
 }
 
 setup_neovim() {
+    echo ">>>>>>>> Installing Neovim"
     git clone --depth 1 --branch v0.9.5 https://github.com/neovim/neovim.git
     cd neovim
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make install
+    echo "Compiling Neovim ........"
+    make CMAKE_BUILD_TYPE=Release > /dev/null
+    echo "Installing Neovim ........"
+    sudo make install > /dev/null
     cd ~
+    echo "Installed Neovim <<<<<<<<<<"
 }
 
 setup_docker() {
-    sudo yum install -y docker
+    echo ">>>>>>>> Setting up Docker"
+    sudo yum install -y -q docker
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     sudo docker run --rm hello-world
-    echo "Finished installing Docker."
+    echo "Installed Docker <<<<<<<<<<"
 }
 
 install_personal_config() {
+    echo ">>>>>>>> Installing xuganyu96.github.io"
     git clone https://github.com/xuganyu96/xuganyu96.github.io.git
-    cd xuganyu96.github.io
     if [ -d ~/.config ]; then
         echo "~/.config already exists"
     else
         mkdir ~/.config
         echo "Created directory ~/.config"
     fi
-    ln -s $(pwd)/neovim ~/.config/nvim
-    ln -s $(pwd)/tmux.conf ~/.tmux.conf
-    ln -s $(pwd)/global.gitignore ~/.gitignore
+    ln -s ~/xuganyu96.github.io/neovim ~/.config/nvim
+    ln -s ~/xuganyu96.github.io/tmux.conf ~/.tmux.conf
+    ln -s ~/xuganyu96.github.io/global.gitignore ~/.gitignore
     git config --global core.excludesFile "~/.gitignore"
     git config --global user.name "Ganyu (Bruce) Xu"
     git config --global user.email "xuganyu@berkeley.edu"
-    cd ~
+    echo "Installed xuganyu96.github.io <<<<<<<<<<"
 }
 
 install_sys_deps() {
+    echo ">>>>>>>> Installing system dependencies"
     sudo yum update -y
-    sudo yum install -y git gcc cmake tmux npm clang
-    sudo yum groupinstall "Development Tools" -y
-    sudo yum install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel tk-devel \
+    sudo yum install -y -q git gcc cmake tmux npm clang
+    sudo yum groupinstall "Development Tools" -y -q
+    sudo yum install -y -q zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel tk-devel \
         libffi-devel xz-devel openssl-devel npm
+    echo "Installed system dependencies <<<<<<<<<<"
 }
 
 setup_all() {
@@ -71,13 +84,11 @@ setup_all() {
     setup_rust
 }
 
-if [[ $1 == "all" ]]; then
-    echo -n "Setup all?"
-    read user_input
-    if [[ $user_input == "Y" ]]; then
-        setup_all
-    else
-        "Setup aborted"
-        exit 0
-    fi
-fi
+export -f install_sys_deps
+export -f install_personal_config
+export -f setup_docker
+export -f setup_neovim
+export -f setup_pyenv
+export -f setup_rust
+export -f setup_all
+
--- devbox/run.sh
+++ devbox/run.sh
@@ -2,19 +2,6 @@
 
 REMOTE_HOST=$1
 
-if [[ -z "${REMOTE_HOST}" ]]; then
-    echo ">>> please enter the remote host"
-    read USER_INPUT
-    REMOTE_HOST=${USER_INPUT}
-fi
-
-echo -n "Setting up ${REMOTE_HOST}? (Y/N)?"
-read USER_INPUT
-if [[ $USER_INPUT != "Y" ]]; then
-    echo "Abort setup"
-    exit 0
-fi
-
 time {
     scp -i $PEM_PATH \
         ./amazon-linux-setup.sh \
@@ -25,6 +12,6 @@ time {
     scp -i $PEM_PATH \
         ~/.ssh/id_rsa.pub \
         ${REMOTE_USER}@${REMOTE_HOST}:/home/${REMOTE_USER}/.ssh/id_rsa.pub
-    ssh -i $PEM_PATH ${REMOTE_USER}@${REMOTE_HOST} "sudo chmod +x ~/setup.sh && sudo ~/setup.sh all"
 }
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  source setup.sh && setup_all"
 ssh -i $PEM_PATH ${REMOTE_USER}@${REMOTE_HOST}
