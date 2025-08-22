5736eb593599cf041698488832e5e23761f91e86
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2025-08-22 16:06:10 -0400

    TODO should be a warning color, not info

--- neovim/init.lua
+++ neovim/init.lua
@@ -227,7 +227,17 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim',    opts = {} },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      keywords = {
+        TODO = { color = "warning" },
+      },
+    },
+  },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
