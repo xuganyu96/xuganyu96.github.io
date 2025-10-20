e640955c4ccabba0d112525028029c87cfff3f80
Ganyu (Bruce) Xu <xuganyu96@gmail.com>
2025-10-20 11:04:54 -0400

    Use full Telescope window for current_buffer_fuzzy_find

--- neovim/init.lua
+++ neovim/init.lua
@@ -420,10 +420,11 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+  -- require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  --   winblend = 10,
+  --   previewer = false,
+  -- })
+  require('telescope.builtin').current_buffer_fuzzy_find()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
