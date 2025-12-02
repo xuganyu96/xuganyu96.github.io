fd576b74a6a41789e0e3269356c74160a2705855
Ganyu (Bruce) Xu <xuganyu96@gmail.com>
2025-12-02 12:57:05 -0500

    parse jinja templates with treesitter, but do not include lsp yet

--- neovim/init.lua
+++ neovim/init.lua
@@ -91,7 +91,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',    tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim',                 tag = 'legacy',      opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       {
@@ -460,7 +460,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'jinja' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
