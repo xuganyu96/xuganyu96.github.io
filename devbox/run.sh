10e7fca7ae9172db84cefddeb7ae73468c13e854
Ganyu (Bruce) Xu <xuganyu96@gmail.com>
2026-05-18 23:03:06 -0400

    Use EC2_xxx prefix

--- devbox/run.sh
+++ devbox/run.sh
@@ -16,14 +16,14 @@ get_variable() {
 }
 
 # Assigning variables
-REMOTE_HOST=$(get_variable "REMOTE_HOST" "Please enter the remote host (IP/hostname): ")
-REMOTE_USER=$(get_variable "REMOTE_USER" "Please enter your username for the remote host: ")
-PEM_PATH=$(get_variable "PEM_PATH" "Please enter the path to your PEM file: ")
+EC2_REMOTE_HOST=$(get_variable "EC2_REMOTE_HOST" "Please enter the remote host (IP/hostname): ")
+EC2_REMOTE_USER=$(get_variable "EC2_REMOTE_USER" "Please enter your username for the remote host: ")
+EC2_PEM_PATH=$(get_variable "EC2_PEM_PATH" "Please enter the path to your PEM file: ")
 
 # Print confirmation
-echo "REMOTE_HOST: ${REMOTE_HOST}"
-echo "REMOTE_USER: ${REMOTE_USER}"
-echo "PEM_PATH: ${PEM_PATH}"
+echo "EC2_REMOTE_HOST: ${EC2_REMOTE_HOST}"
+echo "EC2_REMOTE_USER: ${EC2_REMOTE_USER}"
+echo "EC2_PEM_PATH: ${EC2_PEM_PATH}"
 
 # Confirmation prompt
 read -p "Do you want to proceed with these settings? (yes/no): " confirmation
@@ -35,20 +35,20 @@ fi
 
 # TODO: figure out how to do SSH agent forwarding
 # TODO: figure out how to disable strict host name checking
-scp -i ${PEM_PATH} \
+scp -i ${EC2_PEM_PATH} \
     amazon-linux-setup.sh \
-    ${REMOTE_USER}@${REMOTE_HOST}:/home/ec2-user/setup.sh
+    ${EC2_REMOTE_USER}@${EC2_REMOTE_HOST}:/home/ec2-user/setup.sh
 if [ -f ~/.ssh/id_rsa ]; then
-    scp -i ${PEM_PATH} \
+    scp -i ${EC2_PEM_PATH} \
         ~/.ssh/id_rsa \
-        ${REMOTE_USER}@${REMOTE_HOST}:/home/ec2-user/.ssh/id_rsa
+        ${EC2_REMOTE_USER}@${EC2_REMOTE_HOST}:/home/ec2-user/.ssh/id_rsa
 fi
 if [ -f ~/.ssh/id_ed25519 ]; then
-    scp -i ${PEM_PATH} \
+    scp -i ${EC2_PEM_PATH} \
         ~/.ssh/id_ed25519 \
-        ${REMOTE_USER}@${REMOTE_HOST}:/home/ec2-user/.ssh/id_ed25519
+        ${EC2_REMOTE_USER}@${EC2_REMOTE_HOST}:/home/ec2-user/.ssh/id_ed25519
 fi
-ssh -i ${PEM_PATH} \
-    ${REMOTE_USER}@${REMOTE_HOST} \
+ssh -i ${EC2_PEM_PATH} \
+    ${EC2_REMOTE_USER}@${EC2_REMOTE_HOST} \
     "chmod +x setup.sh && ./setup.sh"
-ssh -i ${PEM_PATH} -L 8080:${REMOTE_HOST}:8080 ${REMOTE_USER}@${REMOTE_HOST}
+ssh -i ${EC2_PEM_PATH} -L 8080:${EC2_REMOTE_HOST}:8080 ${EC2_REMOTE_USER}@${EC2_REMOTE_HOST}
