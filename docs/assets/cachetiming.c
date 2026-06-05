e4f1c94a0c8d6a37d3c4bc279008fdc0cb9cf42f
Ganyu (Bruce) Xu <g66xu@uwaterloo.ca>
2026-06-05 14:35:30 -0400

    Update cache timing detector
    voidkem decapsulation routine has two sub-components: conditional copy
    of a part of the secret key if ciphertext is corrupted; unconditional
    volatile read of a(nother) part of the secret key.
    
    When only the conditional copy is enabled, target (implicit rejection)
    shows strong cache timing channel between corrupted and clean
    ciphertext, while control probes indicate the control region to always
    be a cache miss.
    
    When both the conditional and unconditional read are enabled, target
    only shows cache timing channel __some of the times__, while control
    always show a cache hit.

--- docs/assets/cachetiming.c
+++ docs/assets/cachetiming.c
@@ -4,9 +4,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EPOCHS 1
-#define ROUNDS 5
-#define ROUND_FMT "%" PRIu32 ",%" PRIu32 ",%d,%lu,%lu\n"
+#define EPOCHS 6
+#define ROUNDS 6
+#define CSV_HEADER "  epoch,  round,  corrupted,          target,         control\n"
+#define ROUND_FMT "%7" PRIu32 ",%7" PRIu32 ",%11d,%16zu,%16zu\n"
+
+const static uint64_t dlogprng_mod = 0xFFFFFFFB;
+const static size_t dlogprng_chunk_bytes = 4;
+const static uint64_t dlogprng_base = 65535;
+static uint64_t dlogprng_state = 0;
+
+static void dlogprng_init(void) {
+    dlogprng_state = dlogprng_base;
+}
+
+static uint32_t dlogprng_getrandomu32(void) {
+#if 0
+    fprintf(stderr, "PRNG state = %04x\n", (uint32_t) dlogprng_state);
+#endif
+    uint32_t rand = (uint32_t) dlogprng_state & 0xFFFFFFFF;
+    dlogprng_state = (dlogprng_state * dlogprng_base) % dlogprng_mod;
+    return rand;
+}
 
 typedef struct {
     size_t pklen;
@@ -19,14 +38,14 @@ typedef struct {
 } kemsuite_t;
 
 /* Dummy KEM */
-#define VOIDKEM_PKLEN 64
-#define VOIDKEM_SKLEN 64
-#define VOIDKEM_CTLEN 64
+#define VOIDKEM_PKLEN 2048
+#define VOIDKEM_SKLEN 2048
+#define VOIDKEM_CTLEN 768
 #define VOIDKEM_SSLEN 32
 #define VOIDKEM_SK_REJLOC 0
 #define VOIDKEM_SK_REJLEN 32
-#define VOIDKEM_EXPLOC VOIDKEM_SK_REJLEN
-#define VOIDKEM_REFLOC VOIDKEM_SK_REJLEN
+#define VOIDKEM_EXPLOC VOIDKEM_SK_REJLOC
+#define VOIDKEM_REFLOC (VOIDKEM_SKLEN - 16)
 
 int voidkem_keypair(uint8_t *pk, uint8_t *sk) {
     memset(pk, 0, VOIDKEM_PKLEN);
@@ -45,20 +64,25 @@ int voidkem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk) {
     for (int i = 0; i < VOIDKEM_CTLEN; i++) {
         err |= ct[i];
     }
+#if 1
     if (err) { /* only read rejection symbol on error */
         memcpy(ss, sk + VOIDKEM_SK_REJLOC, VOIDKEM_SK_REJLEN);
     }
-    for (int i = VOIDKEM_SK_REJLEN; i < VOIDKEM_SKLEN; i++) {
+#endif
+#if 0
+    for (int i = VOIDKEM_REFLOC; i < VOIDKEM_SKLEN; i++) {
+        /* another region is always read */
         *(volatile char *)(sk + i);
     }
+#endif
     return 0;
 }
 
 const static kemsuite_t voidkem = {
-    .pklen = 64,
-    .sklen = 64,
-    .ctlen = 64,
-    .sslen = 64,
+    .pklen = VOIDKEM_PKLEN,
+    .sklen = VOIDKEM_SKLEN,
+    .ctlen = VOIDKEM_CTLEN,
+    .sslen = VOIDKEM_SSLEN,
     .crypto_kem_keypair = &voidkem_keypair,
     .crypto_kem_enc = &voidkem_enc,
     .crypto_kem_dec = &voidkem_dec,
@@ -106,13 +130,15 @@ static inline uint64_t probe(void *p) {
 }
 
 /* FIX: randomly generate bits? */
-static inline uint8_t corrupt_bit() { return 0x00; }
+static inline uint8_t corrupt_bit() { 
+    return ((uint8_t) dlogprng_getrandomu32()) & 1;
+}
 
 static int pco_scan(uint32_t epochs, uint32_t rounds, size_t exploc,
                     size_t refloc, const kemsuite_t *kem) {
-    uint8_t *pk, *sk, *ct, *ss, *sscmp;
+    uint8_t *pk, *sk, *ct, *ss, *sscmp, corrupted;
     uint64_t expdur, refdur;
-    int rc = 0, corrupted;
+    int rc = 0;
     pk = malloc(kem->pklen);
     sk = malloc(kem->sklen);
     ct = malloc(kem->ctlen);
@@ -124,17 +150,19 @@ static int pco_scan(uint32_t epochs, uint32_t rounds, size_t exploc,
         goto cleanup;
     }
 
+    fprintf(stderr, CSV_HEADER);
     for (uint32_t epoch = 0; epoch < epochs; epoch++) {
         kem->crypto_kem_keypair(pk, sk);
         for (uint32_t round = 0; round < rounds; round++) {
             kem->crypto_kem_enc(ct, ss, pk);
-            ct[0] ^= corrupt_bit();
+            corrupted = corrupt_bit();
+            ct[0] ^= corrupted;
 
             clflush((void *)(sk + exploc));
             mfence();
             clflush((void *)(sk + refloc));
             mfence();
-            corrupted = kem->crypto_kem_dec(sscmp, ct, sk);
+            kem->crypto_kem_dec(sscmp, ct, sk);
             mfence();
             lfence();
 
@@ -167,7 +195,7 @@ int calibrate(void) {
     uint64_t start, stop, dur;
     *(volatile char *)&val;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 5; i++) {
         dur = probe(&val);
         fprintf(stderr, "hit  = %4lu, ", dur);
 
@@ -181,6 +209,7 @@ int calibrate(void) {
 }
 
 int main(void) {
+    dlogprng_init();
     calibrate();
     pco_scan(EPOCHS, ROUNDS, VOIDKEM_EXPLOC, VOIDKEM_REFLOC, &voidkem);
 
